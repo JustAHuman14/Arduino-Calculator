0d61fdd48cca033dd7e8dd7fe4af3eb6981b39f1
JustAHuman <kashyaparnav671@gmail.com>
2025-12-29 19:36:04 +0530

    Commit 4
    Refactor calculator logic and keypad handling.

--- calculator.ino
+++ calculator.ino
@@ -1,124 +1,39 @@
-#include <Arduino.h>
-#include <LiquidCrystal_I2C.h> 
+#include <LiquidCrystal_I2C.h>
+#include <Keypad.h>
 
-LiquidCrystal_I2C lcd(0x27, 16, 2); 
+const byte rows = 4;
+const byte cols = 4;
+byte rowPins[rows] = {2, 3, 4, 5};
+byte colPins[cols] = {6, 7, 8, 9};
+
+char keys[rows][cols] = {
+    {'1', '2', '3', '+'},
+    {'4', '5', '6', '-'},
+    {'7', '8', '9', '*'},
+    {'C', '0', '=', '/'},
+};
+
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, rows, cols);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 void setup()
 {
   lcd.init();
   lcd.backlight();
-
-  for (int i = 2; i <= 12; i++) 
-  {
-    pinMode(i, INPUT_PULLUP);
-  }
-
-  for (int i = A0; i < A4; i++)
-  {
-    pinMode(i, INPUT_PULLUP);
-  }
 }
 
 String currentNum = "";
+String totalNum = "";
 float num1 = 0;
 float num2 = 0;
 float result = 0;
 bool hasNum1 = false;
 char currentOp;
-char ops[] = {'+', '-', '*', '/', '='};
-int inputDelay = 200;
-void calculate();
 bool calculationComplete = false;
-
-void loop()
-{
-  for (int i = 2; i < 12; i++)
-  {
-    if (digitalRead(i) == LOW)
-    {
-      if (calculationComplete)
-      {
-        lcd.clear(); 
-      }
-      
-      calculationComplete = false;
-      currentNum += String(i - 2);
-      lcd.print(i - 2);
-      delay(inputDelay);
-    }
-  }
-
-  for (int i = 12; i < A4; i++)
-  {
-    if (digitalRead(i) == LOW)
-    {
-      if (i != 13)
-      {
-        if (currentNum != "")
-        {
-          if (!hasNum1)
-          {
-            num1 = currentNum.toFloat();
-            currentOp = ops[i - A0];
-            lcd.print(currentOp);
-            currentNum = "";
-            hasNum1 = true;
-
-            if (i == 12)
-            {
-              calculate();
-              result = num1;
-              lcd.clear();
-              lcd.print(result);
-              hasNum1 = false;
-              calculationComplete = true;
-            }
-          }
-          else
-          {
-            num2 = currentNum.toFloat();
-            calculate();
-
-            if (i != 12)
-            {
-              currentOp = ops[i - A0];
-              lcd.print(currentOp);
-            }
-            else
-            {
-              lcd.clear();
-              lcd.setCursor(0, 0);
-              lcd.print("The Calculator");
-              lcd.setCursor(0, 1);
-              lcd.print("will reset.");
-              delay(2000);
-              lcd.clear();
-              lcd.setCursor(0, 0);
-              lcd.print("If you'll enter");
-              lcd.setCursor(0, 1);
-              lcd.print("any other no.");
-
-              delay(2000);
-              lcd.clear();
-              lcd.noCursor();
-              if (currentOp == '/')
-              
-                lcd.print(result);
-              else
-                lcd.print(result, 0);
-
-              hasNum1 = false;
-              calculationComplete = true;
-            }
-          }
-        }
-      }
-    }
-  }
-}
 
 void calculate()
 {
+  
   if (currentOp == '+')
     num1 += num2;
 
@@ -138,4 +53,114 @@ void calculate()
 
   result = num1;
   currentNum = "";
+}
+
+void loop()
+{
+  if(totalNum.length() == 16) lcd.setCursor(0,1);
+  char key = keypad.getKey();
+
+  if (key)
+  {
+    if (key == 'C')
+    {
+      totalNum = "";
+      lcd.clear();
+      currentNum = "";
+      num1 = 0;
+      num2 = 0;
+      result = 0;
+      hasNum1 = false;
+      calculationComplete = false;
+    }
+
+    if (key >= '0' && key <= '9')
+    {
+      if (calculationComplete)
+      {
+        totalNum = "";
+        lcd.clear();
+        currentNum = "";
+        num1 = 0;
+        num2 = 0;
+        result = 0;
+      }
+
+      calculationComplete = false;
+      currentNum += String(key);
+      totalNum += String(key);
+      lcd.print(key);
+    }
+    else
+    {
+      if (key == '+' || key == '-' || key == '*' || key == '/' || key == '=')
+      {
+        if (currentNum != "")
+        {
+          if (!hasNum1)
+          {
+            num1 = currentNum.toFloat();
+            currentOp = key;
+            totalNum += String(key);
+            lcd.print(currentOp);
+            currentNum = "";
+            hasNum1 = true;
+
+            if (key == '=')
+            {
+              calculate();
+              result = num1;
+              totalNum = "";
+              lcd.clear();
+              
+              if (currentOp == '/')
+              {
+                lcd.print("= ");
+                lcd.print(result);
+              }
+              else
+              {
+                lcd.print("= ");
+                lcd.print(result, 0);
+              }
+              
+              hasNum1 = false;
+              calculationComplete = true;
+            }
+          }
+          else
+          {
+            num2 = currentNum.toFloat();
+            calculate();
+
+            if (key != '=')
+            {
+              currentOp = key;
+              totalNum+=String(key);
+              lcd.print(currentOp);
+            }
+            else
+            {
+              totalNum = "";
+              lcd.clear();
+              
+              if (currentOp == '/')
+              {
+                lcd.print("= ");
+                lcd.print(result);
+              }
+              else
+              {
+                lcd.print("= ");
+                lcd.print(result, 0);
+              }
+              
+              hasNum1 = false;
+              calculationComplete = true;
+            }
+          }
+        }
+      }
+    }
+  }
 }
