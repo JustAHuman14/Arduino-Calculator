2a6583b74a08447d4f1dc5d31b8db1a9b7fad0c2
JustAHuman <kashyaparnav671@gmail.com>
2026-01-05 21:16:03 +0530

    Commit 8

--- calculator.ino
+++ calculator.ino
@@ -58,17 +58,8 @@ void calculate()
   currentNum = "";
 }
 
-int getFreeRam()
-{
-  extern int __heap_start, *__brkval;
-  int v;
-  return (int)&v - (__brkval == 0 ? (int)&__heap_start : (int)__brkval);
-}
-
 void loop()
 {
-  Serial.print("Ram used: ");
-  Serial.println(getFreeRam());
   char key = keypad.getKey();
 
   if (key)
